5b99377eff700f39e520a5ee7b21c1dc29c2a83f
SJ <sungjong@blooming9.com>
2020-04-27 00:59:51 +0900

    make configuration

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gliderifle:closed0813@cluster0-5sttu.gcp.mongodb.net/test?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('mongoDB connected')).catch(err => console.log(err))
-//mongodb+srv://gliderifle:<password>@cluster0-5sttu.gcp.mongodb.net/test?retryWrites=true&w=majority
 
-app.get('/',(req,res) => res.send('Hello world!'))
+
+app.get('/',(req,res) => res.send('Hello world!111'))
+
+
+//route
+app.post('/register',(req,res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({success: true})
+  })
+
+})
+
+
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
